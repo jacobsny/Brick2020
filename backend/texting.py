9837578b888cb272e27a492cd4321ba910367633
charliebrown314 <jwbrown4@buffalo.edu>
2020-02-09 01:37:30 -0500

    post in js

--- backend/texting.py
+++ backend/texting.py
@@ -1,22 +1,27 @@
 import smtplib
 
-from email.message import EmailMessage
 
 def send_ingredients(ingredients, provider, phone_number):
-    msg = EmailMessage()
-    str = "Constellation Cocktails -- Your Ingredients\n"
+    msg = "\nConstellation Cocktails -- Your Ingredients:\n"
     for i in ingredients:
-        str += i + ",\n"
-    str += "Thank you for choosing Constellation Cocktails"
-    msg.set_content(str)
-    provider_to_address = {"Verizon": "@vtext.com"}
-    msg['To'] = phone_number + provider_to_address[provider]
-    msg["From"] = "brickhacks_Joe@gmail.com"
+        msg += i + ",\n"
+    msg += "Thank you for choosing Constellation Cocktails!"
+    provider_to_address = {"Verizon": "@vtext.com", "Alltel": "@message.alltel.com",
+                           "ATT": "@txt.att.net", "Tmobile": "@tmomail.net",
+                           "Virgin": "@vmobl.com", "Sprint": "@messaging.sprintpcs.com",
+                           "Nextel": "@messaging.nextel.com", "UsCell": "@mms.uscc.net"}
+    to_addr = phone_number + provider_to_address[provider]
+    from_addr = "constellationcocktails@gmail.com"
 
-    s = smtplib.SMTP('localhost', 8080)
-    s.send_message(msg)
-    s.quit()
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(from_addr, "JacobIsAnAlcoholic")
+    # print(msg)
+    s.sendmail(from_addr, to_addr, msg)
+    s.close()
 
 
 if __name__ == '__main__':
-    send_ingredients(["sup joe"],"Verizon","7166970452")
+    send_ingredients(["Hello", "John", "Yeet"], "Verizon", "7162204247")
---
+++ frontend/static/js/js.js
@@ -0,0 +1,20 @@
+function post(endpt, args, takesArgs) {
+const http = new XMLHttpRequest();
+const url= "localhost:8080/" + endpt;
+if(takesArgs){
+    const data = new FormData();
+    for(let k in args){
+        data.append(k, args[k]);
+    }
+    http.open("POST", url);
+    http.send(data)
+}
+else {
+    http.open("POST", url);
+    http.send();
+}
+
+http.onreadystatechange = (e) => {
+  return http.responseText
+}
+}
