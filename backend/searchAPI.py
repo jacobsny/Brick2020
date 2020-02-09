dfc27d1d9a92b6eb21b02444bf3d9d49db8c9e7d
jacobsny <jacob.snyderman@gmail.com>
2020-02-08 22:30:49 -0500

    fixed valid_search

--- backend/searchAPI.py
+++ backend/searchAPI.py
@@ -11,28 +11,28 @@ db = client.constellation
 
 def list_cocktails():
     data = db.cocktails.distinct("Name")
-    print(data)
     return data
 
 
 def valid_search(keywords):
-    print(keywords)
     with open("../data/filtered.csv") as types:
         reader = csv.reader(types)
         for i in range(0,3):
             for type in next(reader):
                 if type.lower() in keywords.lower():
                     return True
-    for word in list_cocktails().split():
-        if word.lower() in keywords.lower():
-            return True
+    for cocktail in list_cocktails():
+        for word in keywords.split():
+            if word.lower() in cocktail.lower() and word.lower() != "and" and word.lower() != "the":
+                return True
     return False
     # Is search valid in constellations and cocktailDB?
     # returns a boolean if valid
+
 
 def search_results(keywords, servings):
     print(keywords,servings)
 
 
 if __name__ == '__main__':
-    print(valid_search("beer"))
+    print(valid_search("gin and tonic"))
